bd4f4dfe0bd2651559089d37234476749cae85b9
Dòi <36270825+sonnguyentfr@users.noreply.github.com>
2021-11-24 13:40:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("div.sheet").removeAttr("style");
+});
+'use strict';
+
+function modifyDOM() {
+    var idWidth = $('#idWidth').val();
+    var idHeight = $('#idHeight').val();
+    if (idWidth > 0 && idHeight > 0) {
+        //$('div.sheet').attr('style', 'height:' + idHeight + 'mm; width: ' + idWidth + 'mm');
+        $('div.sheet').css({ 'height': + idHeight * 3.78 +'px' });
+        $('div.sheet').css({ 'width': + idWidth * 3.78 + 'px' });
+    }
+    else {
+        $('div.sheet').css({ 'height': '500px'});
+        $('div.sheet').css({ 'width': '480px'});
+    }
+    return true;
+}
+$('#btnchayde').click(function () {
+    chrome.tabs.executeScript({
+        code: '(' + modifyDOM + ')();'
+    }, function (selection) {
+        alert(selection);
+    });
+});
